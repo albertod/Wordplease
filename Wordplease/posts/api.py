f47c149f8d2f2621131b6dd978e59bb5d7ad0813
Alberto Di Martino <albertodmartino@gmail.com>
2015-07-22 20:13:09 -0400

    Implementando parcialment el PUT method de los post, pero hay problemas con los fields usados en el body del request

--- Wordplease/posts/api.py
+++ Wordplease/posts/api.py
@@ -15,8 +15,6 @@ class BlogsList(generics.ListCreateAPIView):
     serializer_class = BlogSerializer
 
 
-
-
 class PostListAPI(generics.ListAPIView):
     queryset = Post.objects.all()
     serializer_class = PostSerializer
@@ -26,6 +24,21 @@ class PostDetailAPI(generics.GenericAPIView):
 
     permission_classes = (permissions.IsAuthenticatedOrReadOnly, IsOwnerOrReadOnly)
 
+    def get(self, request, pk):
+        post = get_object_or_404(Post, pk=pk)
+        self.check_object_permissions(request, post)  # compruebo si el usuario autenticado puede hacer GET en este user
+        serializer = PostSerializer(post)
+        return Response(serializer.data)
+
+    def put(self, request, pk):
+        post = get_object_or_404(Post, pk=pk)
+        self.check_object_permissions(request, post)  # compruebo si el usuario autenticado puede hacer PUT en este user
+        serializer = PostSerializer(instance=post, data=request.data)
+        if serializer.is_valid():
+            serializer.save()
+            return Response(serializer.data, status=status.HTTP_200_OK)
+        else:
+            return Response(serializer.errors, status=status.HTTP_400_BAD_REQUEST)
 
     def delete(self, request, pk):
         post = get_object_or_404(Post, pk=pk)
--- Wordplease/posts/serializers.py
+++ Wordplease/posts/serializers.py
@@ -11,3 +11,19 @@ class PostSerializer(serializers.ModelSerializer):
     class Meta:
         model = Post
         field = ('id', 'title', 'summary', 'body', 'image_url', 'date_published', 'category', 'blog__user__username')
+
+
+    def update(self, instance, validated_data):
+        """
+        Actualiza una instancia de Post a partir de los datos
+        del dicccionario validated_data que contiene valores deserializados
+        :param instance: objeto Post a actualizar
+        :param validated_data: diccionario con nuevos valores para el Post
+        :return: objeto Post actualizado
+        """
+        instance.title = validated_data.get('title')
+        instance.summary = validated_data.get('summary')
+        instance.body = validated_data.get('body')
+        instance.image_url = validated_data.get('image_url')
+        instance.save()
+        return instance
