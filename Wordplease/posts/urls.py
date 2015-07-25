0967d7391f99337b6ad3224c684e6d1557153e27
Alberto Di Martino <albertodmartino@gmail.com>
2015-07-25 16:46:40 -0400

    Fixed small bug with completition of url in posts/url.py for posts detail (added a '/' at the end of the regex)

--- Wordplease/posts/urls.py
+++ Wordplease/posts/urls.py
@@ -8,14 +8,14 @@ urlpatterns = [
 
     url(r'^$', views.index, name='index'),
     url(r'blogs$', views.blog_list, name='blogs'),
-    url(r'blogs/(?P<username>\w+)$', views.blog_user, name='blog_user'),
-    url(r'blogs/(?P<username>\w+)/(?P<pk>[0-9]+$)', views.post_detail, name='post_detail'),
+    url(r'blogs/(?P<username>\w+)/$', views.blog_user, name='blog_user'),
+    url(r'blogs/(?P<username>\w+)/(?P<pk>[0-9]+$)/', views.post_detail, name='post_detail'),
     url(r'new-post$', views.new_post, name='new_post'),
 
     #API Blogs and posts
     url(r'api/1.0/blogs/$', BlogsList.as_view(), name='blogs_list_api'),
     url(r'api/1.0/posts/$', PostListAPI.as_view(), name='posts_list_api'),
-    url(r'api/1.0/posts/(?P<pk>[0-9]+)$', PostDetailAPI.as_view(), name='post_detail_api'),
+    url(r'api/1.0/posts/(?P<pk>[0-9]+)/$', PostDetailAPI.as_view(), name='post_detail_api'),
 
 
     #API Users
