93ef148d15fcd50a700a129f8d6e1b8ccca5e5c8
Alberto Di Martino <albertodmartino@gmail.com>
2015-08-03 14:47:14 -0400

    fixed small bug with url for list blogs

--- Wordplease/posts/urls.py
+++ Wordplease/posts/urls.py
@@ -7,7 +7,7 @@ from users.api import UserDetailAPI, UserListAPI
 urlpatterns = [
 
     url(r'^$', views.index, name='index'),
-    url(r'blogs$', views.blog_list, name='blogs'),
+    url(r'blogs/$', views.blog_list, name='blogs'),
     url(r'blogs/(?P<username>\w+)/$', views.blog_user, name='blog_user'),
     url(r'blogs/(?P<username>\w+)/(?P<pk>[0-9]+)/$', views.post_detail, name='post_detail'),
     url(r'new-post$', views.new_post, name='new_post'),
